54b59bc02176f9ac6598e2d4bd0805fb41b7ab2d
eloggit <elogtechmail@gmail.com>
2021-03-27 16:57:39 -0400

    added variables to access inquirer and fs, and link to generateMarkdown file. also fixed question array to call inquirer content and started editing initialization functions in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,41 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const generateMarkdown = require(`./utils/generateMarkdown`)
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = ["Description", "Table of Contents", "Installation", "Usage", "License", "Contributing", "Tests", "Questions"];
+// const questions = ["Description", "Table of Contents", "Installation", "Usage", "License", "Contributing", "Tests", "Questions"];
+const questions = [{
+    type: "input",
+    name: "githubUserName",
+    message: "What is your Github username?"
+}, {
+    type: "input",
+    name: "titleOfRepo",
+    message: "What is the title of your project?"
+}, {
+    type: "input",
+    name: "userEmail",
+    message: "What is your email address?"
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) return console.log(err);
+        console.log('Your readme has been created.');
+      });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(data => {
+        // console.log(data)
+        var template = generateMarkdown(data)
+        writeToFile("README.md", template)
+        console.log(template)
+    })
+}
 
 // Function call to initialize app
 init();
