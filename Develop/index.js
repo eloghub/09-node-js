1078dd9a7934b8e64b1e538b6b6c3772301831de
eloggit <elogtechmail@gmail.com>
2021-03-27 22:13:27 -0400

    editing/adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -17,6 +17,31 @@ const questions = [{
     type: "input",
     name: "userEmail",
     message: "What is your email address?"
+}, {
+    type: "input",
+    name: "description",
+    message: "Please describe your project:"
+}, {
+    type: "input",
+    name: "installation",
+    message: "Briefly explain the installation instructions for your project:"
+}, {
+    type: "input",
+    name: "usage",
+    message: "Briefly explain the user information for your project:"
+}, {
+    type: "input",
+    name: "contributing",
+    message: "Briefly explain the contributing guidelines for your project:"
+}, {
+    type: "input",
+    name: "test",
+    message: "Briefly explain the user test instructions for your project:"
+}, {
+    type: "list",
+    name: "license",
+    message: "Select what type of license you want to attach to your project:",
+    // choices:
 }];
 
 // TODO: Create a function to write README file
